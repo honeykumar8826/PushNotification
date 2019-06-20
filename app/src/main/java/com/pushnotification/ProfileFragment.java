86f7aed11d410174f61d20a20f9232483a541438
harishkumar <harishkumar8826@gmail.com>
2019-06-20 19:00:30 +0530

    initial commit

--- app/src/main/java/com/pushnotification/ProfileFragment.java
+++ app/src/main/java/com/pushnotification/ProfileFragment.java
@@ -43,7 +43,7 @@ public class ProfileFragment extends Fragment {
                 mAuth.signOut();
                 Intent intent = new Intent(getActivity(),LoginActivity.class);
                 startActivity(intent);
-                
+
             }
         });
         return view;
